78c2c7ef9055f72bc3246957cc602e31197433da
DarkeNima <naviyaoficial@gmail.com>
2026-03-12 08:44:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
+process.on('uncaughtException', (err) => {
+    console.error('UNCAUGHT EXCEPTION:', err);
+    console.error(err.stack);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('UNHANDLED REJECTION:', reason);
+});
+
 const express = require('express');
 const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
+
+console.log('Starting app on PORT:', PORT);
+
+let server, code;
+try {
+    server = require('./qr');
+    console.log('qr.js loaded OK');
+} catch(e) {
+    console.error('ERROR loading qr.js:', e);
+}
+
+try {
     code = require('./pair');
+    console.log('pair.js loaded OK');
+} catch(e) {
+    console.error('ERROR loading pair.js:', e);
+}
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
+
+if (server) app.use('/server', server);
+if (code) app.use('/code', code);
+
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html')
 })
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
+app.use('/qr', async (req, res, next) => {
+    res.sendFile(__path + '/qr.html')
 })
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html')
 })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star
 
- Server running on http://localhost:` + PORT)
+app.listen(PORT, () => {
+    console.log('Server running on port ' + PORT);
 })
 
 module.exports = app
