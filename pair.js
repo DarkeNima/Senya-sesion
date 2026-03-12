9f2a328b32c439a537125507e35dd97e7177c625
DarkeNima <naviyaoficial@gmail.com>
2026-03-12 11:16:29 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -45,9 +45,10 @@ router.get('/', async (req, res) => {
             if (!sock.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-                const code = await sock.requestPairingCode(num);
+                const code = await sock.requestPairingCode(num, "NAVIYAMD");
                 if (!res.headersSent) {
-                    await res.send({ code });
+                    const formatted = code?.match(/.{1,4}/g)?.join("-") || code;
+                    await res.send({ code: formatted });
                 }
             }
 
